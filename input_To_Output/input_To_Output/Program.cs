9bab9c7323d2aa37bb80cc76bb3e0e4b8c6b2da2
Yeamin_Sheikh <eaminshekh55@gmail.com>
2022-11-06 23:36:43 +0600

    initial commit

---
+++ input_To_Output/input_To_Output/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace input_To_Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+	        {
+                Console.WriteLine("---ENTER INFO---");
+                Console.Write("Institution Name: ");
+                string iName = Console.ReadLine();
+                Console.Write("Student Name: ");
+                string sName = Console.ReadLine();
+                Console.Write("Department: ");
+                string dep = Console.ReadLine();
+                Console.Write("Roll: ");
+                int roll = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\n---FULL INFORMATION--- \nInstitution Name: {0} \nStudent Name: {1} \nDepartment: {2} \nRoll: {3}", iName, sName, dep, roll);
+            }
+	        catch (FormatException e)
+	        {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+	        {
+                Console.WriteLine("Something Went Wrong! Restart the program.");
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
